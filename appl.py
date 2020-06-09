c00efae2a0263a94b1c21094a2f99e62e2f60c3e
Isa <isa.mukhtaroff.farrukh@gmail.com>
2020-06-09 11:23:41 -0700

    Update project config

--- appl.py
+++ appl.py
@@ -9,6 +9,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTION"] = False
 app.secret_key = "isa"
 api = Api(app)
 
